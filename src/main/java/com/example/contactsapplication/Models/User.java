059aa2472767ade672643da889ef2d998facb1a5
Mehtab Kayani <mehtab.kayani@se.experis.com>
2020-08-07 13:19:29 +0200

    added edit functionallity

--- src/main/java/com/example/contactsapplication/Models/User.java
+++ src/main/java/com/example/contactsapplication/Models/User.java
@@ -10,9 +10,9 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @Column(name = "id",nullable = true)
+    //@Column(name = "id",nullable = true)
 
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     private String name;
--- src/main/java/com/example/contactsapplication/controllers/UserController.java
+++ src/main/java/com/example/contactsapplication/controllers/UserController.java
@@ -5,12 +5,14 @@ import com.example.contactsapplication.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class UserController {
 
     @Autowired
@@ -21,6 +23,33 @@ public class UserController {
         List<User> userList = userService.getAllUsers();
         model.addAttribute("userList",userList);
         return "index";
+    }
+
+    @GetMapping("/add")
+    public String addPage(@ModelAttribute User user, Model model) {
+        return "add";
+    }
+
+    @PostMapping("/add")
+    public String add(User user, Model model, RedirectAttributes redirectAttributes){
+
+        userService.addUser(user);
+        return "redirect:/add";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editUser(@PathVariable Integer id, Model model) {
+        User user = userService.getUserById(id);
+        model.addAttribute("user",user);
+        return "edit";
+    }
+
+    @PostMapping("/edit")
+    public String edit(User user,Model model, RedirectAttributes redirectAttributes){
+
+       userService.editUser(user);
+
+        return "redirect:/edit/" + user.getId();
     }
 
 
